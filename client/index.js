ce0e1fddd4691e52960a489d2c981f8ba6e850a0
Henry-Peti <iamgadgetboy@gmail.com>
2020-03-08 09:13:16 +1300

    add html and js code

--- client/index.js
+++ client/index.js
@@ -1,11 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './components/App'
+class Patient {
+  constructor(id, name, location, notes) {
+    this.id = id
+    this.name = name
+    this.location = location
+    this.notes = notes
+  }
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-  )
-})
+class UI {
+  static displayPatient(){
+    const storedPatients = [
+      {
+        id: 1,
+        name: 'john',
+        location: 'room 3',
+        notes: 'Has problem with arm'
+      },
+      {
+        id: 2,
+        name: 'jill',
+        location: 'room 10',
+        notes: 'Has problem with stomach'
+      }
+    ]
+
+    const patients = storedPatients
+
+    patients.forEach((patient) => UI.addPatientToList(patient))
+  }
+
+  static addPatientToList(patient) {
+    const list = document.querySelector('#user-list')
+
+    const row = document.createElement('tr')
+
+    row.innerHTML = `
+        <td>${patient.id}</td>
+        <td>${patient.name}</td>
+        <td>${patient.location}</td>
+        <td>${patient.notes}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">x</a></td>
+    `
+  }
+}
